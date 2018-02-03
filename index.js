de90a9f070f75105e9d7fd797714b2a7af78ddd1
Misan Iwere <temisan.iwere@mail.utoronto.ca>
2018-02-03 18:32:19 -0500

    chat backend set up

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
-
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var users = [];
+var connections = [];
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
@@ -31,8 +33,37 @@ app.get('/', function(req, res) {
         });
 })
 
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  //disconnect
+  socket.on('disconnect', function(data){
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), connections.length);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  //send message
+  socket.on('send message', function(data){
+    console.log('data is ' + data);
+    io.sockets.emit('new message', {msg: data, user: socket.username});
+  });
+
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('get users', users);
+  };
 
 
+});
 //Routes
 
 /*app.get('/applicants', ta.findAll);
@@ -45,5 +76,5 @@ app.get('/courses', ta.findWithCourses);*/
 
 
 // Start the server
-app.listen(3000);
+server.listen(3000);
 console.log('Listening on port 3000');
